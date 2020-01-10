ca65d16da8dd949c78179c85353ae2fafcd0aab4
sapinspys <garri009@fiu.edu>
2020-01-09 21:33:56 -0800

    Implemented CORS

--- airbnb/app.py
+++ airbnb/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from .predict import add_prediction
+from flask_cors
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     @app.route('/')
     def root():
