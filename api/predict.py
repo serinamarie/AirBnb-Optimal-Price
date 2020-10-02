c0f1f868d590727282cccd6f66bf90f971d1c8d3
sapinspys <garri009@fiu.edu>
2020-10-02 14:54:39 -0400

    Cleaning up predict script

--- api/predict.py
+++ api/predict.py
@@ -7,17 +7,10 @@ from sklearn import preprocessing
 # sys.path.append(r'path/to/python module file')
 
 def add_prediction(listing):
-    """
-
-    """
     pkl_filename = "fresh_pickles.pkl"
     with open(pkl_filename, 'rb') as file:
         pickle_model = pickle.load(file)
 
-    # pickle_in = open("ABB.pkl", 'rb')
-    # pickle_model = pickle.load(pickle_in)
-
-    # listing['price'] = 99
     X_train = listing.copy()
     del X_train["id"]
     del X_train["user_id"]
@@ -27,9 +20,6 @@ def add_prediction(listing):
 
     columns = ["host_response_rate","neighbourhood_cleansed","property_type","room_type","accommodates","bathrooms","cleaning_fee","minimum_nights","instant_bookable","kitchen","smoke_detector","self_check_in","hot_water","local_host"]
     data = [[X_train[y] for y in columns]]
-
-    # f_names = pickle_model.feature_names
-    # df = df[f_names]
 
     df = pd.DataFrame(data,columns=columns)
 
@@ -42,20 +32,14 @@ def add_prediction(listing):
     replaced_apartment_type = {'Apartment': 0 , 'House': 1 , 'Hostel':2, 'Hotel':3}
     df['property_type']= df['property_type'].map(replaced_apartment_type).fillna(4)
 
-    # df_2 = pd.get_dummies(df)
-    # X_test = df.iloc[0].values
-
     y_pred = pickle_model.predict(df)
     y_pred = np.exp(y_pred)
 
     listing['predicted_price'] = y_pred[0]
     return str(listing)
 
-
+# ONLY REQUEST WITH THE FOLLOWING:
 # {
-# 	"photo": "https://picsum.photos/400/250?random=1578527625018",
-# 	"title": "Whatever Dude",
-# 	"summary": "This place is legiiiiiit",
 # 	"host_response_rate": 100,
 # 	"neighbourhood_cleansed": "Mitaka Shi",
 # 	"property_type": "Hotel",
@@ -69,7 +53,5 @@ def add_prediction(listing):
 # 	"self_check_in": 1,
 # 	"hot_water": 0,
 # 	"accommodates":0,
-# 	"id": 1578527625018,
-# 	"user_id": 101203042,
 # 	"local_host": 0
 # }
