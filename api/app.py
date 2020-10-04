4af1ca2d72eba11573270f6fdf1c8c43c7b620a1
sapinspys <garri009@fiu.edu>
2020-10-04 14:46:34 -0400

    Final cleanup, response is now only price in JSON format

--- api/app.py
+++ api/app.py
@@ -1,8 +1,7 @@
 #  FLASK_APP=airbnb:APP FLASK_ENV=development flask run
 from flask import Flask, request, jsonify
-from .predict import add_prediction
+from .predict import find_price
 from flask_cors import CORS
-
 
 def create_app():
     app = Flask(__name__)
@@ -10,15 +9,15 @@ def create_app():
 
     @app.route('/')
     def root():
-        return 'Tokyo Airbnb Apartment Price Appraisal Tool'
+        return 'Tokyo Airbnb Price Appraisal Tool'
 
     @app.route('/predict', methods=['POST'])
     def predict():
         try:
             listing = request.get_json(force=True)
-            content = jsonify(add_prediction(listing))
+            content = jsonify(find_price(listing))
         except Exception as identifier:
-            content = str(identifier)
+            content = {"error": str(identifier)}
         return content
 
     return app
--- api/predict.py
+++ api/predict.py
@@ -1,41 +1,38 @@
 import pickle
 import pandas as pd
 import numpy as np
-from sklearn import preprocessing
-from pathlib import Path 
+from pathlib import Path
+import json
 
-# import sys
-# sys.path.append(r'path/to/python module file')
 
-def add_prediction(listing):
-    # pkl_filename = "final_model.pkl"
+def find_price(listing):
     path_to_file = Path("./notebooks/final_model.pkl")
     with open(path_to_file, 'rb') as file:
         pickle_model = pickle.load(file)
 
-    X_train = listing.copy()
-    # del X_train["id"]
-    # del X_train["user_id"]
-    # del X_train["photo"]
-    # del X_train["title"]
-    # del X_train["summary"]
+    features = listing.copy()
+    columns = ["host_response_rate", "neighbourhood_cleansed", "property_type", "room_type", "accommodates", "bathrooms",
+               "cleaning_fee", "minimum_nights", "instant_bookable", "kitchen", "smoke_detector", "self_check_in", "hot_water", "local_host"]
 
-    columns = ["host_response_rate","neighbourhood_cleansed","property_type","room_type","accommodates","bathrooms","cleaning_fee","minimum_nights","instant_bookable","kitchen","smoke_detector","self_check_in","hot_water","local_host"]
-    data = [[X_train[y] for y in columns]]
+    data = [[features[y] for y in columns]]
+    df = pd.DataFrame(data, columns=columns)
 
-    df = pd.DataFrame(data,columns=columns)
+    replaced_room_type = {'Entire home/apt': 0,
+                          'Private room': 1, 'Hotel room': 2, 'Shared room': 3}
+    df['room_type'] = df['room_type'].map(replaced_room_type)
 
-    replaced_room_type = {'Entire home/apt': 0 , 'Private room': 1 , 'Hotel room':2,'Shared room':3 }
-    df['room_type']= df['room_type'].map(replaced_room_type)
-    
-    replaced_neighbourhood_cleansed = {'Sumida Ku': 0 , 'Hino Shi': 1 , 'Chuo Ku':2}
-    df['neighbourhood_cleansed']= df['neighbourhood_cleansed'].map(replaced_neighbourhood_cleansed).fillna(3)
+    replaced_neighbourhood_cleansed = {
+        'Sumida Ku': 0, 'Hino Shi': 1, 'Chuo Ku': 2}
+    df['neighbourhood_cleansed'] = df['neighbourhood_cleansed'].map(
+        replaced_neighbourhood_cleansed).fillna(3)
 
-    replaced_apartment_type = {'Apartment': 0 , 'House': 1 , 'Hostel':2, 'Hotel':3}
-    df['property_type']= df['property_type'].map(replaced_apartment_type).fillna(4)
+    replaced_apartment_type = {'Apartment': 0,
+                               'House': 1, 'Hostel': 2, 'Hotel': 3}
+    df['property_type'] = df['property_type'].map(
+        replaced_apartment_type).fillna(4)
 
     y_pred = pickle_model.predict(df)
     y_pred = np.exp(y_pred)
+    prediction = {'predicted_price': int(y_pred)}
 
-    listing['predicted_price'] = y_pred[0]
-    return str(listing)
+    return prediction
